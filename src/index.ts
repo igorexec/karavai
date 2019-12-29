e8dbb2da8ada0e221fbc591d64ca937b7ee1e7ff
igorexec <igorexec94@gmail.com>
2019-12-29 11:01:43 -0500

    add: promise resolve

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
+import { createImage } from './image';
+
 export default class Karavai {
   private stream: string[];
 
+  private loadedImages: string[] = [];
+
   private canvas: HTMLCanvasElement;
 
-  constructor(stream: string[], canvasRef: HTMLCanvasElement) {
+  private options: object;
+
+  constructor(stream: string[], canvasRef: HTMLCanvasElement, options: object = {}) {
     this.stream = stream;
     this.canvas = canvasRef;
+    this.options = options;
   }
 
-  preloadImages = () => {
+  preloadImages = () => new Promise((resolve, reject) => {
     this.stream.forEach((imgPath) => {
-      const img = new Image();
-      img.src = imgPath;
+      createImage(imgPath, {
+        onLoad: () => {
+          this.onImageLoad(imgPath);
+          if (this.loadedImages.length === this.stream.length) resolve();
+        },
+        onError: (err: Event | string) => reject(err),
+      });
     });
+  });
+
+  private onImageLoad = (imgPath: string) => {
+    this.loadedImages.push(imgPath);
   };
 }
