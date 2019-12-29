5389c4aae9b6fc154eb266c56b6d62b16cb8f88a
igorexec <igorexec94@gmail.com>
2019-12-28 20:26:06 -0500

    add: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
-import path from 'path';
+const path = require('path');
 
-export default {};
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'src/index.js'),
+  },
+
+  mode: isProduction ? 'production' : 'development',
+
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
