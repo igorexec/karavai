0554b67b176fd0c2c87715ecbcd313eed9a44d43
igorexec <igorexec94@gmail.com>
2020-01-08 17:17:06 -0500

    add: add tslint

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,91 @@
-import { createImage } from './image';
-import { KaravaiOptions } from './types';
+import { createImage } from './image'
+import { KaravaiOptions } from './types'
 
+// tslint:disable-next-line:no-default-export
 export default class Karavai {
-  private stream: string[];
+  private loadedImages: string[] = []
 
-  private loadedImages: string[] = [];
+  private canvas: HTMLCanvasElement
 
-  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D | null
 
-  private context: CanvasRenderingContext2D | null;
+  private options: KaravaiOptions
 
-  private options: KaravaiOptions;
-
-  private startPosition = 0;
+  private startPosition = 0
 
   constructor(
-    stream: string[],
-    canvasRef: HTMLCanvasElement,
+    private stream: string[],
+    private canvasRef: HTMLCanvasElement,
     options: KaravaiOptions = { speed: 1 },
   ) {
-    this.stream = stream;
-    this.canvas = canvasRef;
-    this.options = options;
-    this.context = canvasRef.getContext('2d');
+    this.stream = stream
+    this.canvas = canvasRef
+    this.options = options
+    this.context = canvasRef.getContext('2d')
   }
 
-  preloadImages = () => new Promise((resolve, reject) => {
-    const { onImagesLoad } = this.options;
-    this.stream.forEach((imgPath) => {
-      createImage(imgPath, {
-        onLoad: () => {
-          this.onImageLoad(imgPath);
-          if (this.loadedImages.length === this.stream.length) {
-            if (onImagesLoad) onImagesLoad();
-            resolve();
-          }
-        },
-        onError: (err: Event | string) => reject(err),
-      });
-    });
-  });
+  preloadImages = () =>
+    new Promise((resolve, reject) => {
+      const { onImagesLoad } = this.options
+      this.stream.forEach(imgPath => {
+        createImage(imgPath, {
+          onError: (err: Event | string) => reject(err),
+          onLoad: () => {
+            this.onImageLoad(imgPath)
+            if (this.loadedImages.length === this.stream.length) {
+              if (onImagesLoad) {
+                onImagesLoad()
+              }
+              resolve()
+            }
+          },
+        })
+      })
+    })
 
   start = () => {
-    this.startPosition = window.pageYOffset;
-    this.subscribe();
-  };
+    this.startPosition = window.pageYOffset
+    this.subscribe()
+  }
 
   stop = () => {
-    this.unsubscribe();
-  };
+    this.unsubscribe()
+  }
 
   private subscribe = () => {
-    document.addEventListener('scroll', this.onScroll);
-  };
+    document.addEventListener('scroll', this.onScroll)
+  }
 
   private unsubscribe = () => {
-    document.removeEventListener('scroll', this.onScroll);
-  };
+    document.removeEventListener('scroll', this.onScroll)
+  }
 
   private onScroll = () => {
-    const positionFromStart = window.pageYOffset - this.startPosition;
-    const speed = this.options.speed * 30;
-    const nextFrameIndex = Math.round(positionFromStart / speed);
+    const positionFromStart = window.pageYOffset - this.startPosition
+    const speed = this.options.speed * 30
+    const nextFrameIndex = Math.round(positionFromStart / speed)
 
-    const isLastFrame = nextFrameIndex + 1 > this.stream.length;
+    const isLastFrame = nextFrameIndex + 1 > this.stream.length
     if (isLastFrame || nextFrameIndex < 0) {
-      return;
+      return
     }
 
-    this.drawImageOnCanvas(this.loadedImages[nextFrameIndex]);
-  };
+    this.drawImageOnCanvas(this.loadedImages[nextFrameIndex])
+  }
 
   private onImageLoad = (imgPath: string) => {
-    this.loadedImages.push(imgPath);
-  };
+    this.loadedImages.push(imgPath)
+  }
 
   private drawImageOnCanvas = (imgPath: string) => {
-    const image = new Image();
-    image.src = imgPath;
+    const image = new Image()
+    image.src = imgPath
     image.onload = () => {
       if (this.context) {
-        this.canvas.width = image.width;
-        this.canvas.height = image.height;
-        this.context.drawImage(image, 0, 0);
+        this.canvas.width = image.width
+        this.canvas.height = image.height
+        this.context.drawImage(image, 0, 0)
       }
-    };
-  };
+    }
+  }
 }
