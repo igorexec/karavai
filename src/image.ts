97244785542b06385c9ac58b10982d7226b33581
igorexec <icheliadinski@gmail.com>
2020-01-10 22:33:08 -0500

    chg: change styles

--- src/image.ts
+++ src/image.ts
@@ -3,3 +3,10 @@ export const createImage = (url: string) => {
   image.src = url
   return image
 }
+
+export const preloadImage = (imagePath: string): Promise<Event> =>
+  new Promise((resolve, reject) => {
+    const image = createImage(imagePath)
+    image.onload = resolve
+    image.onerror = reject
+  })
