8692cde41cc80b9ef2128276aab7c9e916aa5567
igorexec <igorexec94@gmail.com>
2020-01-18 18:43:33 -0500

    chg: move karavai to lib

---
+++ lib/index.ts
@@ -0,0 +1,66 @@
+import {createImage, preloadImage} from './image'
+import {KaravaiOptions} from './types'
+
+// tslint:disable-next-line:no-default-export
+export default class Karavai {
+  private startPosition = 0
+  private readonly context: CanvasRenderingContext2D | null
+  private cachedImages: Map<string, HTMLImageElement>
+
+  constructor(
+    private images: string[],
+    private canvasRef: HTMLCanvasElement,
+    private options: KaravaiOptions = {threshold: 30},
+  ) {
+    this.context = canvasRef.getContext('2d')
+    this.cachedImages = new Map<string, HTMLImageElement>()
+  }
+
+  preloadImages = () =>
+    Promise.all(
+      this.images.map(imagePath => preloadImage(imagePath).then(img => this.cachedImages.set(imagePath, img))),
+    )
+
+  start = () => {
+    this.startPosition = window.pageYOffset
+    this.drawImageOnCanvas(this.images[0])
+    this.subscribe()
+  }
+
+  stop = () => {
+    this.unsubscribe()
+  }
+
+  private subscribe = () => {
+    document.addEventListener('scroll', this.onScroll)
+  }
+
+  private unsubscribe = () => {
+    document.removeEventListener('scroll', this.onScroll)
+  }
+
+  private onScroll = () => {
+    const positionFromStart = window.pageYOffset - this.startPosition
+    const nextFrameIndex = Math.round(positionFromStart / this.options.threshold)
+
+    const isLastFrame = nextFrameIndex + 1 > this.images.length
+    if (isLastFrame || nextFrameIndex < 0) {
+      return
+    }
+
+    this.drawImageOnCanvas(this.images[nextFrameIndex])
+  }
+
+  private drawImageOnCanvas = (imgPath: string) => {
+    let image = this.cachedImages.get(imgPath)
+
+    if (!image) {
+      image = createImage(imgPath)
+    }
+    this.canvasRef.width = image.width
+    this.canvasRef.height = image.height
+    if (this.context) {
+      this.context.drawImage(image, 0, 0)
+    }
+  }
+}
