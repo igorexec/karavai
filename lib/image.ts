02f25759c28203fb5ddffee117ebbf9df6367669
igorexec <igorexec94@gmail.com>
2020-01-21 22:42:35 -0500

    add: preload

--- lib/image.ts
+++ lib/image.ts
@@ -1,12 +1,7 @@
-export const createImage = (url: string): HTMLImageElement => {
-  const image = new Image()
-  image.src = url
-  return image
-}
-
 export const preloadImage = (imagePath: string): Promise<HTMLImageElement> =>
   new Promise((resolve, reject): void => {
-    const image = createImage(imagePath)
+    const image = new Image()
+    image.src = imagePath
     image.onload = (): void => resolve(image)
     image.onerror = reject
   })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import Karavai from 'lib'
 
-((): void => {
+(async (): Promise<void> => {
   const images = Array.from({length: 40}, (_, idx) => `/img/${idx}.jpg`)
   const canvas: HTMLCanvasElement | null = document.querySelector('#canvas')
 
   if (canvas) {
     const karavai = new Karavai(images, canvas)
-    // await karavai.preloadImages()
+    await karavai.preloadImages()
     karavai.start()
   }
 })()
