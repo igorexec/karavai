00d0c0a60eeed396b2184273aaef9e3bb3e67491
igorexec <igorexec94@gmail.com>
2020-01-23 15:15:14 -0500

    add: new images

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import Karavai from 'lib'
 
 (async (): Promise<void> => {
-  const images = Array.from({length: 40}, (_, idx) => `/img/${idx}.jpg`)
+  const images = Array.from({length: 300}, (_, idx) => `/img/${idx}.jpg`)
   const canvas: HTMLCanvasElement | null = document.querySelector('#canvas')
 
   if (canvas) {
     const karavai = new Karavai(images, canvas)
-    // await karavai.preloadImages()
+    await karavai.preloadImages(30)
     karavai.start()
   }
 })()
